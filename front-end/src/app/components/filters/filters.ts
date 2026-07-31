fcbf883b9439450cfdf7f4f48379c6074793bae5
Milenaslv20 <mi.projects.tec@hotmail.com>
2026-07-31 16:41:59 -0400

    feature: filter

---
+++ front-end/src/app/components/filters/filters.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-filters",
+  standalone: true,
+  imports: [],
+  templateUrl: "./filters.html",
+  styleUrl: "./filters.scss",
+})
+export class Filters {}
--- front-end/src/app/components/products/products.ts
+++ front-end/src/app/components/products/products.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { Filters } from "../filters/filters";
+import { Product } from "../product/product";
+import { Menu } from "../menu/menu";
 
 @Component({
   selector: "app-products",
-  imports: [],
+  imports: [Filters, Product, Menu],
   templateUrl: "./products.html",
   styleUrl: "./products.scss",
 })
