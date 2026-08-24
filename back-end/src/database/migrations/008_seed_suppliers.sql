e8ba11c3f6f5f699e2e963ee71b4b264bf1db169
Milenaslv20 <milenaslvmedeiros@hotmail.com>
2026-08-23 20:48:02 -0400

    fix: change supplier name error

--- back-end/src/database/migrations/008_seed_suppliers.sql
+++ back-end/src/database/migrations/008_seed_suppliers.sql
@@ -1,11 +1,11 @@
-INSERT INTO supplier (name, contact, cnpj, category)
+INSERT INTO suppliers (name, contact, cnpj, category)
 SELECT v.name, v.contact, v.cnpj, v.category
 FROM (
     VALUES
         ('Atack', '(92)98123-4567', '12.345.678/0001-90', 'Alimentício'),
         ('Infostore', '(92)99234-5678', '98.765.432/0001-12', 'Eletrônicos'),
-        ('Concorde', '(92)98345-6789', '55.555.555/0001-55', 'Papelaria'),
+        ('Concorde', '(92)98345-6789', '55.555.555/0001-55', 'Papelaria')
 ) AS v(name, contact, cnpj, category)
 WHERE NOT EXISTS (
-    SELECT 1 FROM supplier p WHERE p.name = v.name
+    SELECT 1 FROM suppliers p WHERE p.name = v.name
 );
